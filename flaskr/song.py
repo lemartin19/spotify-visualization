435eceb6215868c4a506c14c9112acd180c8c02f
lemartin19 <martin.ly@husky.neu.edu>
2019-02-09 16:12:20 -0500

    double check login and add error handling
    checks for a connection error to Spotify and flashes a warning in such a case. refreshes the token

--- flaskr/song.py
+++ flaskr/song.py
@@ -1,17 +1,23 @@
 import  spotipy
+from flaskr.auth import login_required
 from flask import (
     Blueprint, redirect, render_template, request, session, url_for, app
 )
+from requests.exceptions import SSLError
 
 bp = Blueprint('song', __name__, url_prefix='/')
 
 @bp.route('/song', methods=['GET'])
+@login_required
 def song():
     sp = spotipy.Spotify(auth=session['token'])
     id = request.args.get('id')
-    analysis = sp.audio_analysis(id)
-    features = sp.audio_features(id)
-    track = sp.track(id)
+    try:
+        analysis = sp.audio_analysis(id)
+        features = sp.audio_features(id)
+        track = sp.track(id)
+    except SSLError as err:
+        flash("Connection error - please try again.")
 
     return render_template('song.html', name=session['display_name'], track=track,
       features=features[0], analysis=analysis)
